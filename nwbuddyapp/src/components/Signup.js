2bff5412d191ec6b5252ea56175c673b5f3435fb
Seungmin <smhwang0715@gmail.com>
2019-01-26 21:49:30 -0800

    sign up page in progress

--- nwbuddyapp/src/components/Signup.js
+++ nwbuddyapp/src/components/Signup.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
-import './Signup.css'
+import './Signup.css';
+import {Button,FormControl,Label} from 'react-bootstrap';
 
 class Signup extends Component {
     render() {
         return(
           <div>
-            <div className="signup">
-              hi i'm the signup page
-            </div> <br />
             <div>
               <form name="signupForm" action="/" method="post">
-                Name: <input type="text" name="username" /> <br />
-                Gender: <br />
-                <input type="radio" name="gender" value="male" checked /> Male <br />
-                <input type="radio" name="gender" value="female" /> Female <br />
-                <input type="radio" name="gender" value="other" /> Other <br />
-                Age: <input type="text" name="age" />
+                <div class="form-group">
+                  <label for="name">Name:</label>
+                  <input type="text" name="username" />
+                </div> <br />
+                <div class="form-group">
+                  <label for="gender">Gender:</label> <br />
+                  <input class="form-radio" type="radio" name="gender" value="male" checked /> Male <br />
+                  <input class="form-radio" type="radio" name="gender" value="female" /> Female <br />
+                  <input class="form-radio" type="radio" name="gender" value="other" /> Other <br />
+                </div> <br />
+                <div class="form-group">
+                  <label for="age">Age:</label>
+                  <input type="text" name="age" />
+                </div> <br />
                 <input type="submit" value="Submit" />
               </form>
             </div>
