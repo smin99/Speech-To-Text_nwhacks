93d48d0359d236e4b8cd1c391f5474d5da3172e7
Kyler Griffith <lockheedmartian@gmail.com>
2019-01-27 08:50:38 -0800

    Updates

---
+++ precache-manifest.94aa298bfae005cf88f18fa50ada5b8f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "47fd02cec36eb4875ea4",
+    "url": "/nwBuddy/static/css/main.f2381bdd.chunk.css"
+  },
+  {
+    "revision": "47fd02cec36eb4875ea4",
+    "url": "/nwBuddy/static/js/main.47fd02ce.chunk.js"
+  },
+  {
+    "revision": "00e019592267379bb2e1",
+    "url": "/nwBuddy/static/js/1.00e01959.chunk.js"
+  },
+  {
+    "revision": "eb76eda8f533395678fc",
+    "url": "/nwBuddy/static/js/runtime~main.eb76eda8.js"
+  },
+  {
+    "revision": "e024dfeef0c3c79142bfe69e7d2b0f9c",
+    "url": "/nwBuddy/static/media/beard-boy-casual-1222271.e024dfee.jpg"
+  },
+  {
+    "revision": "b0998dfd7a1912db1940be93cdec4eac",
+    "url": "/nwBuddy/static/media/pexels-photo-1315741.b0998dfd.jpeg"
+  },
+  {
+    "revision": "1f2fd0271b58abbd46e9f2069af32444",
+    "url": "/nwBuddy/static/media/adult-beard-boy-220453.1f2fd027.jpg"
+  },
+  {
+    "revision": "150b0524e7879c7a85b4b92c0447f384",
+    "url": "/nwBuddy/static/media/adult-blur-cars-724887.150b0524.jpg"
+  },
+  {
+    "revision": "21d3399801dbc0b2ba09cac1df3af582",
+    "url": "/nwBuddy/static/media/beautiful-brunette-cute-774909.21d33998.jpg"
+  },
+  {
+    "revision": "0d22890f162067ea28133c8de890ddfc",
+    "url": "/nwBuddy/index.html"
+  }
+];
