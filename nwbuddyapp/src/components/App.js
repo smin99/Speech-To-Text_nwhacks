668f16bffe7ba2b8d9743217559cabb5ece2948d
Kyler Griffith <lockheedmartian@gmail.com>
2019-01-26 18:19:22 -0800

    update

---
+++ nwbuddyapp/src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Navbar from './Navbar';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div>
+          Hi this is an accountability buddy finder app;
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ nwbuddyapp/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Navbar.css'
+
+class Navbar extends Component {
+    render() {
+        return(
+            <div className="navbar">
+                hi I'm a navbar
+            </div>
+        )
+    }
+}
+
+export default Navbar;
