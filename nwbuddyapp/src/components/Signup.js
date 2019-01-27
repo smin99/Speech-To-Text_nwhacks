413e4809f8d643a6a6fea8c693ccd7dc95a553f6
Seungmin <smhwang0715@gmail.com>
2019-01-26 19:31:08 -0800

    signup page

--- nwbuddyapp/src/components/Signup.js
+++ nwbuddyapp/src/components/Signup.js
@@ -4,9 +4,22 @@ import './Signup.css'
 class Signup extends Component {
     render() {
         return(
+          <div>
             <div className="signup">
-                hi i'm the signup page
+              hi i'm the signup page
+            </div> <br />
+            <div>
+              <form name="signupForm" action="/" method="post">
+                Name: <input type="text" name="username" /> <br />
+                Gender: <br />
+                <input type="radio" name="gender" value="male" checked /> Male <br />
+                <input type="radio" name="gender" value="female" /> Female <br />
+                <input type="radio" name="gender" value="other" /> Other <br />
+                Age: <input type="text" name="age" />
+                <input type="submit" value="Submit" />
+              </form>
             </div>
+          </div>
         )
     }
 }
